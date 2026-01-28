6fed84cc0310878b011d9e44c237e5fec3841763
Syrae <suraee30@gmail.com>
2026-01-28 21:01:54 +0530

    Completed day 12

--- day12/day12_exercises/day12.py
+++ day12/day12_exercises/day12.py
@@ -40,3 +40,67 @@ def rgb_color_gen():
         colour.append(int(random.random()*256))
     return colour
 print(rgb_color_gen())
+
+# hexa decimal color generator
+"""Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array (six hexadecimal numbers written after #. 
+Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples)."""
+
+def hex_colour_generator(num):
+    hex_char = "abcdef"
+    list_of_colours =[]
+    for _ in range(num):
+        colour = "#"+"".join(random.choice(string.hexdigits[:16])  for _ in range(6))
+        list_of_colours.append(colour)
+    return list_of_colours
+print(hex_colour_generator(6))
+    
+
+"""Write a function list_of_rgb_colors which returns any number of RGB colors in an array."""
+
+def list_of_rgb_colours(num):
+    colours =[]
+    for _ in range(num):
+        colours.append(rgb_color_gen())
+    return colours
+
+print(list_of_rgb_colours(6))
+
+#Write a function generate_colors which can generate any number of hexa or rgb colors.
+
+def generate_colours(type_of_code,num_of_colours):
+    type_of_code=type_of_code.lower()
+    colours=[]
+    if type_of_code == "rgb" :
+        for _ in range (num_of_colours):
+            colours.append(rgb_color_gen())
+    elif type_of_code =="hex":
+        colours.extend(hex_colour_generator(num_of_colours))
+    else :
+        return "Invalid Type"
+    return colours
+
+print(generate_colours("rgb",2))
+print(generate_colours("hex",2))
+
+# level 3
+#Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+lst=[0,1,2,3,4,5,6,7]
+def shuffle_list(lst):
+    shuffle =[]
+    while not(len(lst))==len(shuffle):
+        x = random.choice(lst)
+        if x not in shuffle:
+            shuffle.append(x)
+    return shuffle
+print(shuffle_list(lst))
+
+#Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+def list_of_seven_random():
+    random_seven =[]
+    while not len(random_seven)==7 :
+         x = random.randint(0,9)
+         if x not in random_seven:
+             random_seven.append(x)
+    return random_seven
+
+print(list_of_seven_random())
